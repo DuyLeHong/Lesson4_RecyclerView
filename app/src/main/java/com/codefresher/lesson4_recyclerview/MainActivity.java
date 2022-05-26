248a5a290beb6a54a88bc6818eb28a5cb4782a35
Duy Le <lehongduy.bk@gmail.com>
2022-05-26 21:04:38 +0700

    update recyclerview - add menu

--- app/src/main/java/com/codefresher/lesson4_recyclerview/MainActivity.java
+++ app/src/main/java/com/codefresher/lesson4_recyclerview/MainActivity.java
@@ -1,13 +1,17 @@
 package com.codefresher.lesson4_recyclerview;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -39,5 +43,64 @@ public class MainActivity extends AppCompatActivity {
         rclView.setAdapter(studentAdapter);
         rclView.setLayoutManager(linearLayoutManager);
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_modify_3:
+                //Sửa đội phần tử số 3
+                Student student = arrDatas.get(2);
+                student.setsName("TÊN MỚI SỐ 3: "
+                        + Calendar.getInstance().getTimeInMillis());
+
+                long millis = System.currentTimeMillis();
+
+                student.setAge(2000);
+
+                //studentAdapter.notifyDataSetChanged();
+                studentAdapter.notifyItemChanged(2);
+
+                //adapter.notifyDataSetChanged();
+                return true;
+
+            case R.id.menu_insert_2:
+                //Thêm một sinh viên mới vào vị trí số 2
+                Student newStudent = new Student("SINH VIÊN 2:"
+                        + Calendar.getInstance().getTimeInMillis(), 1990);
+                arrDatas.add(1, newStudent);
+                studentAdapter.notifyItemInserted(1);
+                return true;
+
+            case R.id.menu_remove_first:
+                //Xóa sinh viên ở vị trí đầu tiên
+                arrDatas.remove(0);
+                studentAdapter.notifyItemRemoved(0);
+                return true;
+            case R.id.menu_new_7:
+                //Danh sách 7 sinh viên mới
+
+                arrDatas.clear();//Xóa bỏ danh sách cũ
+
+                //Thêm 7 sinh viên mới
+                for (int i = 1; i <= 7; i++)
+                    arrDatas.add(new Student("SV Mới " + i, 1990 + i));
+
+                //Thông báo toàn bộ dữ liệu thay đổi
+                studentAdapter.notifyDataSetChanged();
+
+                return true;
+
+            default:
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
